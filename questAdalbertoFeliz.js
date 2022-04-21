e52b77e0341080c290e9ca9e2826e4a47eac1d8b
Beatriz Teixeira <beatrizteisi@gmail.com>
2022-04-21 17:16:43 -0300

    ajustes no código da quest 2 e inclusão da quest 3 do Adalberto

--- questAdalbertoFeliz.js
+++ questAdalbertoFeliz.js
@@ -2,7 +2,7 @@ function startQuestDois() {
     var escolhaDois=prompt ("Adalberto chamou a ambulância e se atrasou para o trabalho.\n Ao chegar no trabalho ele tem duas opções:\n1-  Chama o chefe e explica o motivo do atraso\n2- Entra na reunião sem explicações sobre o atraso");
 
     if (escolhaDois==1) {
-        alert ("Adalberto chamou o chefe em um momento em que ele não estava falando na reunião e explicou o ocorrido, justificando o atraso, ao terminar a reunião o chefe o chama no escritório e ele consegue a promoção, por que o chefe valoriza o relacionamento dele como os subordinados e valoriza a conduta tomada. Adalberto volta ao hospital e conta para a companheira que foi promovido.\n\n\nPARABÉNS VOCÊ AJUDOU UM LAXCADO!");
+        alert ("Adalberto chamou o chefe em um momento em que ele não estava falando na reunião e explicou o ocorrido, justificando o atraso. Ao terminar a reunião o chefe o chama no escritório e ele consegue a promoção, por que o chefe valoriza o relacionamento dele com os subordinados e valoriza a conduta tomada. Adalberto volta ao hospital e conta para a companheira que foi promovido.\n\n\nPARABÉNS VOCÊ AJUDOU UM LAXCADO!");
 
         var deNovo;
         while (deNovo != 1 && deNovo != 2) {
@@ -16,7 +16,7 @@ function startQuestDois() {
             }
         }
     } else if (escolhaDois == 2) {
-        window.location.href="./questAdalbertoTrabalhoTriste.html";
+        window.location.href="./questAdalbertoTrabalho.html";
     } else {
         alert ("Essa opção não é válida, tente novamente.");
     }
---
+++ questAdalbertoTrabalho.js
@@ -0,0 +1,25 @@
+function startQuestTres() {
+    var escolhaTres=prompt ("Na reunião ele passa por uma situação terrível, onde o chefe o humilha publicamente, o que Adalberto faz:\n1- Recebe a humilhação, não fala nada e se retira da sala de reuniões\n2- Se desculpa pelo atraso e informa eufórico para todos da sala que sua companheira entrou em trabalho de parto");
+
+    if (escolhaTres==1) {
+        alert ("Após a reunião ele é chamado na sala do chefe, e recebe a notícia de que está sendo demitido. Adalberto não se comunicou corretamente com a liderança, ninguém sabia da sua situação e ele foi demitido!\n\n\n ADALBERTO DE LAXCOU!");
+    } else if (escolhaTres == 2) {
+        alert ("Adalberto é chamado na sala do chefe que se sentiu ofendido com a conduta em reunião, e exige que para não o demitir que Adalberto demita 5 funcionários de sua equipe. Adalberto demite os 5 funcionários, não consegue a promoção e agora tem que entregar o mesmo resultado com 5 funcionários a menos!\n\n\n ADALBERTO E OS FUNCIONÁRIOS SE LAXCARAM!");
+        var deNovoJob;
+            while (deNovoJob != 1 && deNovoJob != 2) {
+                deNovoJob = prompt ("Tentar novamente? Digite:\n1-sim\n2-não");
+                if(deNovoJob==1){
+                    window.location.href="./cardLaxcados.html";
+                }else if(deNovoJob==2){
+                    window.location.href="./index.html";
+                } else {
+                    alert ("Essa opção não é válida, tente novamente.");
+                }
+            }
+    } else {
+        alert ("Essa opção não é válida, tente novamente.");
+    }
+}
+
+var botaoComecarQuestTres = document.getElementById("botaoComecarQuestTres");
+botaoComecarQuestTres.onclick = startQuestTres;
